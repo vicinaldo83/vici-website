32582e90e84293e32a03d2ed771d57850cb8a63b
Victoria Matos <higormatosvici@gmail.com>
2025-11-26 03:12:42 -0300

    Static build configuration

--- next.config.js
+++ next.config.js
@@ -1,21 +1,8 @@
-const { webpack } = require('next/dist/compiled/webpack/webpack');
-
-// @ts-check
-const isProd = process.env.NODE_ENV === "development";
-
-
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    basePath: (isProd) ? "" : ""
+    output: 'export',
+    distDir: 'dist',
 }
 
-module.exports = nextConfig,
-{
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ["@svgr/webpack"],
-        });
-        return config;
-    },
-};
+module.exports = nextConfig
